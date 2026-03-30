9b95ddfd19f8f6b47059eee80f438711ec88216a
Guilherme Soares May Rios <136922444+guilhermerios21@users.noreply.github.com>
2026-03-30 23:08:30 +0000

    primeiro commit

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 from markupsafe import Markup 
 
 #db_connect = create_engine('mysql://root:my-password@db/my_database')
-db_connect = create_engine('sqlite:///mydb.db', echo=True)
+db_connect = create_engine('sqlite:///../mydb.db', echo=True)
 
 app = Flask(__name__)
 
